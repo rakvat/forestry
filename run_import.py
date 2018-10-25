e976dbc059f83812da3cfdbc546246412b86b2a5
rakvat <rakvat@users.noreply.github.com>
2018-10-25 02:53:36 -0700

    rm dataset

--- run_import.py
+++ run_import.py
@@ -1,12 +1,9 @@
 from base_mysql_importer import BaseMysqlImporter
-from dataset_importer import DatasetImporter
 
 def main():
     base_mysql_importer = BaseMysqlImporter()
     base_mysql_importer.run()
 
-    dataset_importer = DatasetImporter()
-    dataset_importer.run()
 
 if __name__ == '__main__':
     main()
